6035940e13ebb7192bf25ddcf583c6b8a286c748
ektabuneti123 <bunetikrishna@gmail.com>
2026-08-12 19:31:03 +0530

    Add files via upload

---
+++ Practice1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//Printing message on the screen
+int main(){
+printf("Hello World!\n");
+printf("Hello Ekta.\n");
+printf("I am Learning C Language\n");
+printf("Hello World\n");
+
+return 0;
+}
