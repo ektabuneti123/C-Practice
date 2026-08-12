ff929399fe8e196163176062b5c2b3323a72da11
ektabuneti123 <bunetikrishna@gmail.com>
2026-08-12 20:37:02 +0530

    Add files via upload

---
+++ Practice2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main(){
+//Q1. Declare an integer variable age and store your age in it. Display the age.
+int age = 18;
+printf("The age stored in the variable is: %d\n\n", age);
+
+//Q2. Declare a float variable price and store a value in it. Display the price.
+float price = 25.26;
+printf("The value of the price is:%.2f\n\n", price);
+
+//Q3. Declare a char variable grade and store your grade in it. Display the grade.
+char grade = 'A';
+printf("The Grade is: %c\n\n", grade);
+
+//Q4. Declare three variables to store your age, roll number, and grade, and display all three.
+int age_1 = 17;
+int roll_no = 89;
+char Gr_de = 'A';
+
+printf("%d\n", age_1);
+printf("%d\n", roll_no);
+printf("%c\n\n", Gr_de);
+
+
+//Q5. Declare two integer variables a and b, store values in them, and display their values.
+int a = 15;
+int b = 16;
+printf("The value of a is : %d\n", a);
+printf("The value of b is : %d\n\n", b);
+
+
+//Q6. Declare variables for student ID, student age, student fee, and student grade, and display all four values.
+int student_id = 1;
+int student_age = 18;
+int student_fee = 45000;
+char student_grade = 'A';
+printf("Student ID: %d\n", student_id);
+printf("Student age: %d\n", student_age);
+printf("Student fee: %d\n", student_fee);
+printf("Student Grade: %d\n", student_grade);
+return 0;
+}
